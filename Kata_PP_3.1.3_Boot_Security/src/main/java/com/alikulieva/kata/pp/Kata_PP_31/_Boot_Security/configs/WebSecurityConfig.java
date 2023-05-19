c93178335b4ea190c010a9609133bdc5adf2cfa4
Olga Askerova <oleckaask@gmail.com>
2023-05-19 11:41:49 +0300

    1

--- Kata_PP_3.1.3_Boot_Security/src/main/java/com/alikulieva/kata/pp/Kata_PP_31/_Boot_Security/configs/WebSecurityConfig.java
+++ Kata_PP_3.1.3_Boot_Security/src/main/java/com/alikulieva/kata/pp/Kata_PP_31/_Boot_Security/configs/WebSecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
@@ -56,8 +57,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
-    protected PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
